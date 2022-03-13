3fbad213b3c8ac0948db5e771607ec3f0fa9dce5
Will <williammibus@gmail.com>
2022-03-13 13:23:23 +1030

    second try at connecting database

--- inc/dbconn.inc.php
+++ inc/dbconn.inc.php
@@ -1,15 +1,12 @@
 <?php
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "Assignment2_Tolkein"); // Group name postfixed
-define("DB_USER", "dbadmin");
-define("DB_PASS", "");
-
-$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if (!$conn) {
-    // Something went wrong...
-    echo "Error: Unable to connect to database.<br>";
-    echo "Debugging errno: " . mysqli_connect_errno() . "<br>";
-    echo "Debugging error: " . mysqli_connect_error() . "<br>";
-    exit;
-}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
